3a540f47391880df7e582d41b7d6218748a73b12
HetavP2 <hetav.j.patel@gmail.com>
2024-09-14 09:26:07 -0400

    second commit with initial components needed

---
+++ app/components/fact-check.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FactCheck = () => {
+  return <div>Fact-Check</div>;
+};
---
+++ app/components/reel-text.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const ReelText = () => {
+  return (
+    <div>ReelText</div>
+  );
+};
