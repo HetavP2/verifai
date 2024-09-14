f97b6a2f395d8ee44a98154431577f68056e8c90
HetavP2 <hetav.j.patel@gmail.com>
2024-09-14 12:50:31 -0400

    video fetched

--- app/components/reel.jsx
+++ app/components/reel.jsx
@@ -1,20 +1,37 @@
-// import fetchVideo from '@/functions/fetchVideo';
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
 
 export const Reel = () => {
-    // fetchVideo()
-    return (
-      <div className="col-span-5 sm:col-span-2 bg-primary">
-        Reel
+  const [videoId, setVideoId] = useState("");
+  return (
+    <div className="col-span-5 sm:col-span-2 bg-primary">
+      Reel
+      <div className="w-full text-center m-10">
+        <input
+          type="text"
+          value={videoId}
+          onChange={(e) => setVideoId(e.target.value)}
+          placeholder="Enter a YouTube video id..."
+          class="rounded-none rounded-l-lg input input-bordered w-2/3 sm:w-1/3 inline"
+        />
+        <button class="btn btn-neutral inline border-2 rounded-none rounded-r-lg">
+          Submit
+        </button>
+      </div>
+      {videoId ? (
         <iframe
-          className="w-full m-10 h-auto"
-          src="https://www.youtube.com/embed/{ VIDEOID }?si=Z4i21SJAJt_qsPQr"
+          className="w- h-auto"
+          src={`https://www.youtube.com/embed/${videoId}?si=Z4i21SJAJt_qsPQr`}
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerpolicy="strict-origin-when-cross-origin"
           allowfullscreen
         ></iframe>
-      </div>
-    );
-}
+      ) : (
+        <h1>Hi there</h1>
+      )}
+    </div>
+  );
+};
--- app/page.js
+++ app/page.js
@@ -2,14 +2,12 @@ import Image from "next/image";
 import { FactCheck } from "./components/fact-check";
 import { Reel } from "./components/reel";
 import { ReelText } from "./components/reel-text";
-import { Input } from "./components/input";
 
-export default function Home() {
+export default function Home({videoId}) {
   return (
     <div>
-      <Input />
       <div className="grid grid-cols-5 my-10 text-center mx-10">
-        <Reel />
+        <Reel videoId={videoId}/>
         <ReelText />
         <FactCheck />
       </div>
